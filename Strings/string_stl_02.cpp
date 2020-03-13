2934e90760fb16d3eff62a9d6d79a5420c240796
shady2002 <60711070+shady2002@users.noreply.github.com>
2020-03-13 23:36:45 +0530

    my_commit_1

--- Strings/string_stl_02.cpp
+++ Strings/string_stl_02.cpp
@@ -3,11 +3,12 @@
 #include<string>
 using namespace std;
 
+//comparator function
 bool compare(string a,string b){
     if(a.length()==b.length()){
-        return a<b;
+        return a<b;//increasing order
     }
-    return a.length()>b.length();
+    return a.length()>b.length(); //decreasing order
 }
 
 int main() {
@@ -15,16 +16,16 @@ int main() {
     cin>>n;
     cin.get();
 
-    string s[100]; //Vector
+    string s[100]; //Vector could be used in place of this array
     
-    for(int i=0;i<n;i++){
-        getline(cin,s[i]);
-    }
+    for(int i=0;i<n;i++)
+        getline(cin,s[i]); //this getline function comes in handy and should be preferred over cin>> in case of strings as with getline you can 
+                           //specify the delimiter (default is '\n'),  cin >> uses any whitespace character as a delimiter this includes spaces, newlines, tabs etc.
+    
 
     sort(s,s+n,compare);
 
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++)
         cout<<s[i]<<endl;
-    }
 
 }
